63ecece7552dc00878808b8ffa738c9560e23e9d
BurnElmo <81187003+BurnElmo@users.noreply.github.com>
2022-08-30 19:52:11 +0200

    Implemented inout and basic options

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from pptx import Presentation
 
-pres = Presentation()
+print("Keep in mind, the maximum header count, and the entire program might not be fully accurate since this is my first attempt at webscraping")
 
 # Add input functionality with the amount of links or command line arguments
-url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+url = input("Enter a valid Wikipedia URl: ")
+print(url)
+
+count = 0
+maxCount = int(input("Enter the maximum amount of parsed headers: "))
 
 request_results = requests.get(url)
 
@@ -13,19 +16,18 @@ request_results = requests.get(url)
 
 wiki_page = BeautifulSoup(request_results.text, "html.parser")
 
+paragraphs = ""
+
 # Title:
 # wiki_page.find("h1", {"id": "firstHeading"}).getText()
 # Paragraphs:
 # for paragraph in wiki_page.select('p'):
 #     print(paragraph.getText())
 
-slide1_register = pres.slide_layouts[1]
-slide1 = pres.slides.add_slide(slide1_register)
-title1 = slide1.shapes.title
-subtitle1 = slide1.placeholders[1]
-title1.text = wiki_page.find("h 1", {"id": "firstHeading"}).getText()
-subtitle1.text = ""
-for paragraph in wiki_page.select('p'):
-    subtitle1.text += paragraph.getText()
+with open('pararaphs.txt', 'w') as f:
+    f.write(wiki_page.find("h1", {"id": "firstHeading"}).getText() + "\n\n\n")
 
-pres.save("Presentation.pptx")
+    for paragraph in wiki_page.select('p'):
+        if count < maxCount:
+            f.write(paragraph.getText())
+            count += 1
